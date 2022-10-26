df8db7f14bb814cf7891ccb67f7c6dd6a5d51ea8
Winston Limanto <wmlimanto@gmail.com>
2022-10-25 19:45:38 -0700

    add main component for each todo item

---
+++ client/src/components/ToDo.js
@@ -0,0 +1,40 @@
+// main component for each todo item
+
+import React from 'react';
+import { 
+  Card, 
+  CardContent, 
+  Container, 
+  Grid, 
+  Typography, 
+  IconButton
+}from '@mui/material';
+import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+
+const ToDo = () => {
+  return (
+    <div>
+      <Container>
+        <Card 
+          variant="outlined"
+          sx={{ mt: 3, background: "#eceff1" }}
+        >
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              <IconButton>
+                <DoneRoundedIcon sx={{ color: "green" }} />
+              </IconButton>
+              test 1
+              <IconButton sx={{ float: "right" }}>
+                <DeleteRoundedIcon sx={{ color: "red" }} />
+              </IconButton>
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </div>
+  )
+};
+
+export default ToDo
