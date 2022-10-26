ea0b579d10e5b7c6966f22230236d4f39add3b23
Winston Limanto <wmlimanto@gmail.com>
2022-10-25 19:46:08 -0700

    add container that contains all the todo items

---
+++ client/src/components/ToDoList.js
@@ -0,0 +1,14 @@
+// container that contains all the todo items
+
+import React from 'react';
+import ToDo from './ToDo';
+
+const ToDoList = () => {
+  return (
+    <div>
+      <ToDo />
+    </div>
+  )
+};
+
+export default ToDoList
